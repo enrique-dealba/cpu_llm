bfaf0ceb84b55a4e8e40f1d700e285ad4b45ce5e
enrique-dealba <enrique.dealba@e-o.solutions>
2023-12-13 10:57:37 -0600

    LLM CPU Redo
    LLM CPU Redo

---
+++ llm_cpu.py
@@ -0,0 +1,22 @@
+from llama_cpp import Llama
+
+model_path_1 = "/home/edealba/Testing/TestingLLMs/models/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/mistral-7b-instruct-v0.1.Q5_K_M.gguf"
+model_path_2 = "/edealba/Testing/TestingLLMs/models/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/mistral-7b-instruct-v0.1.Q5_K_M.gguf"
+model_path_3 = "/Testing/TestingLLMs/models/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/mistral-7b-instruct-v0.1.Q5_K_M.gguf"
+model_path_4 = "/models/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/mistral-7b-instruct-v0.1.Q5_K_M.gguf"
+
+model = Llama(model_path=model_path_1)
+
+system_message = "You are a helpful assistant"
+user_message = "Generate a list of 5 funny dog names"
+
+prompt = f"""<s>[INST] <<SYS>>
+{system_message}
+<</SYS>>
+{user_message} [/INST]"""
+
+max_tokens = 200
+
+output = model(prompt, max_tokens=max_tokens, echo=True)
+
+print(f"LLM: {output}")
